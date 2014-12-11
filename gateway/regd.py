f39be072b326691c214ac80d21e29abfe32c0e19
hadertjou <dacrymn@gmail.com>
2014-12-10 22:18:10 -0500

    gateway

--- gateway/regd.py
+++ gateway/regd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 from flask import Flask, request
+import MySQLdb
 
 """
 A simple script to test the registration process.
@@ -16,5 +17,27 @@ def reg_client():
     print(retstr)
     return retstr
 
+
+# Create a connection object and create a cursor
+Con = MySQLdb.Connect(host="127.0.0.1", port=3306, user="dac_user", passwd="myPassword", db="tst")
+Cursor = Con.cursor()
+
+# Make SQL string and execute it
+sql = "SELECT * FROM Users"
+Cursor.execute(sql)
+
+# Fetch all results from the cursor into a sequence and close the connection
+Results = Cursor.fetchall()
+Con.close()
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=False)
+    
+
+
